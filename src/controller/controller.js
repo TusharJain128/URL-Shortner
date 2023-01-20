7ede0704a9ed87b3a328992dd1c5c39e3d797314
TusharJain128 <tusharjain128@gmail.com>
2023-01-20 22:15:17 +0530

    final

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -2,8 +2,8 @@ const urlModel = require('../urlModel/urlModel')
 const validUrl = require('valid-url')
 const shortid = require('shortid')
 const redis = require('redis')
-const { promisify } = require('util')
-
+const axios= require('axios')
+const {promisify}  = require('util')
 
 
 const redisClient = redis.createClient(
@@ -18,7 +18,7 @@ redisClient.on("connect", async function () {
     console.log("connected to redis")
 })
 
-const SET_ASYNC = promisify(redisClient.SET).bind(redisClient)
+const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient)
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient)
 
 
@@ -28,8 +28,11 @@ exports.createUrl = async function (req, res) {
     try {
         let url = req.body.longUrl
         if (!url) return res.status(400).send({ status: false, msg: "Please enter longUrl" })
+        if(typeof(url) !== "string") return res.status(400).send({status: false, msg: "url should be string"})
         url = url.trim()
         if (!validUrl.isUri(url)) return res.status(400).send({ status: false, msg: "Please enter valid url" })
+        const checkUrl = await axios.get(url).then(()=> url).catch(()=> null)
+        if(checkUrl==null) return res.status(400).send({status: false, msg: "url is not valid"})
         let isUrlExistCatch = await GET_ASYNC(`${url}`)
         if (isUrlExistCatch) {
             let objectConversion = JSON.parse(isUrlExistCatch)
@@ -37,14 +40,14 @@ exports.createUrl = async function (req, res) {
         }
         let isUrlExist = await urlModel.findOne({ longUrl: url })
         if (isUrlExist) {
-            await SET_ASYNC(`${url}`, JSON.stringify(isUrlExist))
+            await SET_ASYNC(`${url}`,60*1440, JSON.stringify(isUrlExist))
             return res.status(200).send({ status: true, msg: isUrlExist })
         }
         let obj = { longUrl: url }
         obj.urlCode = shortid.generate()
         obj.shortUrl = `https://localhost:3000/${obj.urlCode}`
         const savedData = await urlModel.create(obj)
-        await SET_ASYNC(`${url}`, JSON.stringify(savedData));
+        await SET_ASYNC(`${url}`,60*1440, JSON.stringify(savedData));
         res.status(201).send({ status: true, msg: savedData })
     }
 
@@ -64,7 +67,7 @@ exports.getUrl = async function (req, res) {
         }
         let url = await urlModel.findOne({ urlCode: data })
         if (!url) return res.status(404).send({ status: false, msg: "Url not found" })
-        await SET_ASYNC(`${data}`, JSON.stringify(url))
+        await SET_ASYNC(`${data}`,60*1440, JSON.stringify(url))
         res.status(302).redirect(url.longUrl)
     }
     catch (err) {
